7786a00ff93ecfc3056d758fed73adac4176a48b
Ryan Trottier <themeisme@gmail.com>
2017-02-01 15:36:41 -0700

    TypeError: descriptor '__init__' of 'super' object needs an argument

--- ws_chains.py
+++ ws_chains.py
@@ -37,7 +37,7 @@ class WSBulkChain(SpinCustomChain):
         names          = ['0_pre_converge', '1_rough_converge', '2_nospin_eig', '3_get_eigenvalues', '4_final_converge', final_step]
         nupdown_functionals = [pre_converge, bad_converge_gamma, get_nopsin_eig_gamma, get_eigenvalues_gamma, final_converge_gamma]
         super().__init__([pre_converge, bad_converge, get_nopsin_eig, get_eigenvalues, final_converge, hse],
-                                nupdown_functionals=nupdown_functionals, nupdowns=nupdowns, names=names)
+                         nupdown_functionals=nupdown_functionals, nupdowns=nupdowns, names=names)
 
 class WSBulkChain_ferro(CustomChain):
     def __init__(self, vaspobj: Vasp):

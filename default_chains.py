770310b8693b75618574fa79a5e92d62f4660ba2
Ryan Trottier <themeisme@gmail.com>
2017-02-24 11:21:20 -0700

    set kpar automatically

--- default_chains.py
+++ default_chains.py
@@ -342,7 +342,24 @@ def set_kpar_auto(vasp: Vasp, structure=None):
                 return vasp
             else:
                 kpar = kpar -1
+    return vasp
 
+def set_kpar_auto_npar_2(vasp: Vasp, structure=None):
+    nodes = int(os.environ['PBS_NUM_NODES'])
+    np = int(os.environ['PBS_NP'])
+    atoms = len(structure)
+    vasp.npar = 2
+    if np / atoms > 1:
+        kpar = math.ceil(np/atoms)
+        kpoint_str = vasp.kpoints.split('\n')[3]
+        kpoints = [ int(x) for x in kpoint_str.split() ]
+        num_kpoints = kpoints[0] * kpoints[1] * kpoints[2]
+        while kpar > 1:
+            if num_kpoints % kpar == 0 and nodes % kpar ==0:
+                vasp.add_keyword('kpar', kpar)
+                return vasp
+            else:
+                kpar = kpar -1
     return vasp
 
 ##############

872762841b6b6df9995504a01c345ecb99cbb8d9
Ryan Trottier <themeisme@gmail.com>
2017-03-03 09:07:14 -0700

    pbe singlepoing

--- defect_chains.py
+++ defect_chains.py
@@ -90,10 +90,11 @@ class DefectHSE(CustomChain):
     def __init__(self, vaspobj: Vasp, standard=[], override=[], final_step='5_hse' ):
         standard = [load_default_vasp, bulk_standard, load_species, set_333, set_nkred_333, set_kpar_auto, set_iopt_7] + standard
         pbe = CustomFunctional(Vasp, standard)
+        pbe_single = CustomFunctional(Vasp, standard + [single_point, all_output])
         hse = CustomFunctional(Vasp, standard + [hse06])
         hse_single = CustomFunctional(Vasp, standard + [hse06, single_point, all_output])
-        names = ['1_pbe', '2_hse', '3_hse_singlepoint']
-        super().__init__([pbe, hse, hse_single], names=names, vaspobj=vaspobj)
+        names = ['1_pbe', '2_pbe_singlepoint', '3_hse', '4_hse_singlepoint']
+        super().__init__([pbe, pbe_single, hse, hse_single], names=names, vaspobj=vaspobj)
 
 def bulk_standard(vasp: Vasp, structure):
     # Electronic

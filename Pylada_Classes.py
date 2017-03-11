729c06a6eedee9f7a2bf0da8f029f5e0e4c8bdc7
rtrottie <themeisme@gmail.com>
2017-03-11 16:16:06 -0700

    pmg_to_pylada

--- Pylada_Classes.py
+++ Pylada_Classes.py
@@ -17,3 +17,16 @@ def pylada_to_pmg(pylada_structure):
         write.poscar(pylada_structure, name, vasp5=True)
         pymatgen_structure = Poscar.from_file(name).structure # pymatgen.core.Structure
     return pymatgen_structure
+
+def pmg_to_pylada(pmg_structure):
+    '''
+    Converts a pylada structure to a pymatgen structure via a temporary file
+
+    :param pylada_structure: Pylada structure to convert
+    :return: Structure
+    '''
+    with tempfile.NamedTemporaryFile() as tmp:
+        name = tmp.name
+        pmg_structure.to('poscar', name)
+        pylada_structure = read.poscar(name)
+    return pylada_structure

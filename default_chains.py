6e0ab86400eb324b3636e6810520767f544c99e8
rtrottie <themeisme@gmail.com>
2017-07-02 20:16:08 -0600

    added npar_1

--- default_chains.py
+++ default_chains.py
@@ -458,6 +458,10 @@ def set_npar_2(vasp: Vasp, structure=None):
     vasp.npar = 2
     return vasp
 
+def set_npar_1(vasp: Vasp, structure=None):
+    vasp.npar = 1
+    return vasp
+
 def set_isym_0(vasp: Vasp, structure=None):
     vasp.isym =  0
     return vasp

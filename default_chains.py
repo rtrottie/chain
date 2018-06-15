6edd4a1ff5a1bd3bd62c734e0cc665de6b095b0b
Ryan Trottier <themeisme@gmail.com>
2018-06-15 11:59:42 -0600

    more likley to buffer out

--- default_chains.py
+++ default_chains.py
@@ -85,7 +85,7 @@ class CustomChain(object):
             vasp.encut = self.kwargs['encut']
         if 'kpoints' in self.kwargs:
             vasp.kpoints = "Automatic\n0\nAuto\n{}".format(self.kwargs['kpoints'])
-            density = vasp.kpoints
+            density = self.kwargs['kpoints']
             num_kpoints = math.ceil(density / np.linalg.norm(structure.cell[:, 0])) * math.ceil(density / np.linalg.norm(structure.cell[:, 1])) * math.ceil(density / np.linalg.norm(structure.cell[:, 2]))
             if num_kpoints < 4 and (vasp.ismear == -4 or vasp.ismear == -5 or vasp.ismear == 'metal' or vasp.ismear == 'tetra'):
                 vasp.ismear = 0

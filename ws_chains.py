6ea92fb439920aeeaa9fd1cdaa5f2a090c6c2a93
Ryan Trottier <themeisme@gmail.com>
2019-03-05 14:23:41 -0700

    us tetrahedron single point

--- ws_chains.py
+++ ws_chains.py
@@ -508,8 +508,8 @@ misc_labels {}
 class WSBulkSCAN(OptimizedParametersChain):
     def __init__(self, vaspobj: Vasp, bandgap:float=None, standard=[], override=[], final_step='5_hse', incar_override={}, **kwargs):
         standard = [load_default_vasp, load_optimized_species_no_U, cell_relax, herc_bulk, scan, set_kpar_by_core]
-        pbe = CustomFunctional(Vasp, standard)
-        pbe_single = CustomFunctional(Vasp, standard + [all_output])
+        pbe = CustomFunctional(Vasp, standard + override)
+        pbe_single = CustomFunctional(Vasp, standard + [single_point, all_output, tetrahedron] + override)
         names = ['1_scan', '2_scan_reconverge']
         super().__init__([pbe, pbe_single], bandgap=bandgap, names=names, vaspobj=vaspobj, incar_override=incar_override, **kwargs)
 
